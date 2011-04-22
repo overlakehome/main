08025b01c44b26a00fb00bcc42e994388b7f553d
Jeongim Bae <overlakehome@gmail.com>
2011-04-21 21:44:37 -0700

    clean ups

--- oyster/src/devo/Oyster.java
+++ oyster/src/devo/Oyster.java
@@ -223,12 +223,12 @@ public class Oyster {
         // 1.8. Assume you have a method isSubstring which checks if one word is a substring of another.
         //      Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring
         //      (i.e., "waterbottle" is a rotation of "erbottlewat").
-        public static boolean isRotation(String s1, String s2){
+        public static boolean rotated(String s1, String s2){
             if (s1.length() != s2.length()) {
                 return false;
+            } else {
+                return (s1 + s1).contains(s2);
             }
-            String s = s2 + s2;
-            return isSubString(s, s1);
         }
 
         // 1-A. Find the max sum sub array.
