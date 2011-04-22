f25c471c0869e6eff2e2ebfc92e4175cda0e4b4f
Jeongim Bae <overlakehome@gmail.com>
2011-04-21 21:41:28 -0700

    added zeroOutRowsAndColumns

--- oyster/src/devo/Oyster.java
+++ oyster/src/devo/Oyster.java
@@ -195,20 +195,25 @@ public class Oyster {
         }
 
         // 1.7. Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column is set to 0.
-        public static void setZeros(int[][] matrix){
-            int[] row = new int[matrix.length];
-            int[] column = new int[matrix[0].length];
-            for (int i = 0; i < matrix.length; i++){
-                for (int j = 0; j < matrix[0].length; j++){
-                    if (matrix[i][j] == 0){
-                        row[i] = 1;
-                        column[j] = 1;
+        public static void zeroOutRowsAndColumns(int[][] matrix){
+            BitSet rows = new BitSet(matrix.length);
+            BitSet columns = new BitSet(matrix[0].length);
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    if (columns.length() != matrix[i].length) {
+                        throw new IllegalArgumentException("'matrix' must be a rectable.");
+                    }
+
+                    if (matrix[i][j] == 0) {
+                        rows.set(i);
+                        columns.set(i);
                     }
                 }
             }
-            for (int i = 0; i < matrix.length; i++){
-                for (int j = 0; j < matrix[0].length; j++){
-                    if (row[i] == 1 || column[j] == 1){
+
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[0].length; j++) {
+                    if (rows.get(i) || columns.get(j)) {
                         matrix[i][j] = 0;
                     }
                 }
