2f9ab740c42ae8379c2f280b047cf6ce913cd927
Jeongim Bae <overlakehome@gmail.com>
2011-06-22 21:00:56 -0700

    wired up textView1 with place[0]'s name

--- apps/Locals/src/com/overlakehome/locals/Main.java
+++ apps/Locals/src/com/overlakehome/locals/Main.java
@@ -2,6 +2,7 @@ package com.overlakehome.locals;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +10,13 @@ public class Main extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        TextView tv = (TextView)findViewById(R.id.textView1);
+        Place[] places;
+        try {
+            places = Places.findNearby(0, 0, "query", 0, 0);
+            tv.setText(places[0].getName());
+        } catch (Exception e) {
+            tv.setText(e.toString());
+        }
     }
 }
