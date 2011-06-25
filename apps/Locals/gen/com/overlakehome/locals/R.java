3e39401ba8dacdf8a8cb40c9c07d1a8d2dde2de3
Jeongim Bae <overlakehome@gmail.com>
2011-06-25 16:29:13 -0700

    adds HotPlacesActivity and layout.

--- apps/Locals/gen/com/overlakehome/locals/R.java
+++ apps/Locals/gen/com/overlakehome/locals/R.java
@@ -11,14 +11,28 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int ic_tab_bookmarks=0x7f020000;
+        public static final int ic_tab_bookmarks_selected=0x7f020001;
+        public static final int ic_tab_bookmarks_unselected=0x7f020002;
+        public static final int ic_tab_deals=0x7f020003;
+        public static final int ic_tab_deals_selected=0x7f020004;
+        public static final int ic_tab_deals_unselected=0x7f020005;
+        public static final int ic_tab_hotplaces=0x7f020006;
+        public static final int ic_tab_hotplaces_selected=0x7f020007;
+        public static final int ic_tab_hotplaces_unselected=0x7f020008;
+        public static final int ic_tab_places=0x7f020009;
+        public static final int ic_tab_places_selected=0x7f02000a;
+        public static final int ic_tab_places_unselected=0x7f02000b;
+        public static final int icon=0x7f02000c;
     }
     public static final class id {
         public static final int locals01=0x7f050000;
         public static final int locals02=0x7f050001;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int hotplacesactivity=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int placesactivity=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- apps/Locals/src/com/overlakehome/locals/HotPlacesActivity.java
+++ apps/Locals/src/com/overlakehome/locals/HotPlacesActivity.java
@@ -1,17 +1,71 @@
 package com.overlakehome.locals;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class HotPlacesActivity extends Activity {
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-		
-		TextView textview = new TextView(this);
-		textview.setText("This is the HotPlaces tab");
-		setContentView(textview);
-	}
+public class HotPlacesActivity extends ListActivity implements LocationListener{
+
+    private LocationManager locationManager = null;
+    private Place[] places;
+    private double lat = 47.59755;
+    private double lng = -122.32775;
+    private String[] hotplacelist = new String[20];
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        try {
+            places = Places.Foursquare.findNearby(lat, lng, null, 50, 50);
+            for (int i = 0; i < 20; i++) {
+                hotplacelist[i] = places[i].getName();
+            }
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();
+        }
+        
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.hotplacesactivity, hotplacelist));
+
+        ListView lv = getListView();
+        lv.setTextFilterEnabled(true);
+
+        lv.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // When clicked, show a toast with the TextView text
+                Toast.makeText(getApplicationContext(), ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
 }
