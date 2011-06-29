0487c21aa97fc08ba365e4855523ba7a5349a31e
Jeongim Bae <overlakehome@gmail.com>
2011-06-29 00:41:49 -0700

    yata, yata, yata.

--- apps/Locals/src/com/overlakehome/locals/PlacesActivity.java
+++ apps/Locals/src/com/overlakehome/locals/PlacesActivity.java
@@ -1,21 +1,16 @@
 package com.overlakehome.locals;
 
 import android.app.Activity;
-import android.content.Context;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.overlakehome.locals.common.Place;
 import com.overlakehome.locals.common.Places;
 
-public class PlacesActivity extends Activity implements LocationListener {
+public class PlacesActivity extends Activity {
     
     private TextView locals01;
     private TextView locals02;
-    private LocationManager locationManager = null;
     private double lat = 47.59755;
     private double lng = -122.32775;
     private Place[] places;
@@ -26,14 +21,6 @@ public class PlacesActivity extends Activity implements LocationListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.placesactivity);
         locals01 = (TextView)findViewById(R.id.locals01);
-        
-        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 0, this);
-
-        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        if (null != location) {
-            onLocationChanged(location);
-        }
 
         try {
             places = Places.Foursquare.findNearby(lat, lng, null, 50, 50);
@@ -41,30 +28,5 @@ public class PlacesActivity extends Activity implements LocationListener {
         } catch (Exception e) {
             locals01.setText(e.toString());
         }
-    }
-    // https://sites.google.com/site/androidhowto/how-to-1/using-the-gps
-    @Override
-    public void onLocationChanged(Location location) {
-        lat = location.getLatitude();
-        lng = location.getLongitude();
-        locals02 = (TextView)findViewById(R.id.locals02);
-        try {
-            places = Places.Foursquare.findNearby(lat, lng, null, 50, 50);
-            locals02.setText(places[1].getName());
-        } catch (Exception e) {
-            locals02.setText(e.toString());
-        }
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
     }
 }
