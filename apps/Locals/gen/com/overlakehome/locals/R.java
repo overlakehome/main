dea91de2b1c35ce8a87295375515139279ec3d2c
Jeongim Bae <overlakehome@gmail.com>
2011-07-10 23:16:30 -0700

    moved to deals details activity.

--- apps/Locals/gen/com/overlakehome/locals/R.java
+++ apps/Locals/gen/com/overlakehome/locals/R.java
@@ -36,24 +36,24 @@ public final class R {
     public static final class id {
         public static final int dealsListView1=0x7f050001;
         public static final int dealsTextView1=0x7f050000;
-        public static final int detailsAddress=0x7f050010;
-        public static final int detailsDistance=0x7f050011;
-        public static final int detailsFinePrint=0x7f05000f;
-        public static final int detailsImage=0x7f05000c;
-        public static final int detailsMessage=0x7f05000e;
-        public static final int detailsName=0x7f05000d;
-        public static final int detailsPhone=0x7f050012;
-        public static final int detailsWebsite=0x7f050013;
-        public static final int image=0x7f050002;
-        public static final int placesImage=0x7f050008;
-        public static final int placesListView1=0x7f050007;
-        public static final int placesText1=0x7f050009;
-        public static final int placesText2=0x7f05000a;
-        public static final int placesText3=0x7f05000b;
-        public static final int placesTextView1=0x7f050006;
-        public static final int text1=0x7f050003;
-        public static final int text2=0x7f050004;
-        public static final int text3=0x7f050005;
+        public static final int detailsAddress=0x7f050006;
+        public static final int detailsDistance=0x7f050007;
+        public static final int detailsFinePrint=0x7f050005;
+        public static final int detailsImage=0x7f050002;
+        public static final int detailsMessage=0x7f050004;
+        public static final int detailsName=0x7f050003;
+        public static final int detailsPhone=0x7f050008;
+        public static final int detailsWebsite=0x7f050009;
+        public static final int image=0x7f05000a;
+        public static final int placesImage=0x7f050010;
+        public static final int placesListView1=0x7f05000f;
+        public static final int placesText1=0x7f050011;
+        public static final int placesText2=0x7f050012;
+        public static final int placesText3=0x7f050013;
+        public static final int placesTextView1=0x7f05000e;
+        public static final int text1=0x7f05000b;
+        public static final int text2=0x7f05000c;
+        public static final int text3=0x7f05000d;
         public static final int trendsImage=0x7f050016;
         public static final int trendsListView1=0x7f050015;
         public static final int trendsText1=0x7f050017;
@@ -63,13 +63,14 @@ public final class R {
     }
     public static final class layout {
         public static final int deals=0x7f030000;
-        public static final int deals_row=0x7f030001;
-        public static final int main=0x7f030002;
-        public static final int places=0x7f030003;
-        public static final int places_row=0x7f030004;
-        public static final int special_details=0x7f030005;
-        public static final int trends=0x7f030006;
-        public static final int trends_row=0x7f030007;
+        public static final int deals_details=0x7f030001;
+        public static final int deals_row=0x7f030002;
+        public static final int main=0x7f030003;
+        public static final int places=0x7f030004;
+        public static final int places_row=0x7f030005;
+        public static final int special_details=0x7f030006;
+        public static final int trends=0x7f030007;
+        public static final int trends_row=0x7f030008;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
